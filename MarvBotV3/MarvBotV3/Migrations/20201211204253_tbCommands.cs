59c5c913e1e9177fbace818d7e8d41db69bfefad
Marivan Ebrahimi <marivan9507@gmail.com>
2020-12-11 21:43:31 +0100

    Added new commands table

---
+++ MarvBotV3/MarvBotV3/Migrations/20201211204253_tbCommands.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MarvBotV3.Migrations
+{
+    public partial class tbCommands : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TbCommandsLog",
+                columns: table => new
+                {
+                    ID = table.Column<ulong>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserID = table.Column<ulong>(nullable: false),
+                    Username = table.Column<string>(maxLength: 64, nullable: true),
+                    Command = table.Column<string>(nullable: true),
+                    TimeStamp = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TbCommandsLog", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TbCommandsLog");
+        }
+    }
+}
